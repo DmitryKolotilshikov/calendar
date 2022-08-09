03f237ae7e35cc2517e595a75427eddb893d967f
Dmitry Kolotilshikov <57001255+DmitryKolotilshikov@users.noreply.github.com>
2022-08-09 14:33:54 +0500

    fixed week days

--- utils.js
+++ utils.js
@@ -4,13 +4,13 @@ export function addBasicTableLayout(table, month, year) {
     table.insertAdjacentHTML('afterbegin', `
         <thead>
             <tr>
-                <th>пн</th>
-                <th>вт</th>
-                <th>ср</th>
-                <th>чт</th>
-                <th>пт</th>
-                <th>сб</th>
-                <th>вс</th>
+                <th>Mon</th>
+                <th>Tue</th>
+                <th>Wed</th>
+                <th>Thu</th>
+                <th>Fri</th>
+                <th>Sat</th>
+                <th>Sun</th>
             </tr>
         </thead>
         <tbody>
